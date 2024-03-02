42e71e1ac0736dffce986309691ea784782569f3
Chil's Coding World <chilhoank@yahoo.ca>
2024-03-02 12:57:15 -0500

    include myProject

---
+++ MyProject/src/com/example/MyClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MyClass {
+	
+	public void doSomething() {
+		System.out.println("Do something in myClass :"+getClass().getName());
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my Class in Jar");
+	}
+	
+
+}
