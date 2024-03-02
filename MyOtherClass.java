4374c885b2bd1e20582888273e521aadc5ec15d3
Chil's Coding World <chilhoank@yahoo.ca>
2024-03-02 12:23:17 -0500

    myother commit
    myother commit

---
+++ MyOtherClass.java
@@ -0,0 +1,19 @@
+import com.example.MyClass;
+
+public class MyOtherClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyClass myObject = new MyClass();
+		myObject.doSomething();
+	//	myObject.doSomething();
+		
+		
+		MyOtherClass myOtherObject = new MyOtherClass();
+		myOtherObject.test();
+	}
+	public void test() {
+		System.out.println("teseting...");
+	}
+
+}
